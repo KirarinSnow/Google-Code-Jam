439e3e7821a4cd0f3596be99bcc7ef1efc864ba0
KirarinSnow <kirarinsnow@uchicago.edu>
2010-07-22 16:38:25 -0500

    cleanup

--- Round 3 2008/A.py
+++ Round 3 2008/A.py
@@ -3,79 +3,62 @@
 # Problem: How Big Are the Pockets?
 # Language: Python
 # Author: KirarinSnow
-# Usage: python thisfile.py < input.in > output.out
+# Usage: python thisfile.py <input.in >output.out
 
-import sys
-from math import *
-from string import *
-
-MAX = 100000000000
-
-
-def grab():
-    return int(file.readline())
-
-def grabs():
-    return map(int,file.readline().split())
-
-def removechars(str, chars):
-    return str.translate(maketrans('',''),chars)
-
-def table(height, width):
-    t = []
-    for i in range(height):
-	t.append([])
-	for j in range(width):
-	    t[i].append(0)
-    return t
 
 def compute():
-    def adv(x,y,o):
-        if o==0:
+    def adv(x, y, o):
+        if o == 0:
             if y not in d:
                 d[y] = []
             d[y].append(x)
-            return (x,y+1,o)
-        elif o==1:
+            return (x, y+1, o)
+        elif o == 1:
             if x not in e:
                 e[x] = []
             e[x].append(y)
-            return (x+1,y,o)
-        elif o==2:
+            return (x+1, y, o)
+        elif o == 2:
             if y-1 not in d:
                 d[y-1] = []
             d[y-1].append(x)
-            return (x,y-1,o)
+            return (x, y-1, o)
         else:
             if x-1 not in e:
                 e[x-1] = []
             e[x-1].append(y)
-            return (x-1,y,o)
+            return (x-1, y, o)
 
-    def left(x,y,o):
-        return (x,y,(o-1)%4)
-    def right(x,y,o):
-        return (x,y,(o+1)%4)
+    def left(x, y, o):
+        return (x, y, (o-1)%4)
 
-    l = int(buffer.pop(0))
+    def right(x, y, o):
+        return (x, y, (o+1)%4)
+
+    l = input()
     cur = ""
-    for i in range(l):
-        st = buffer.pop(0)
-        count = int(buffer.pop(0))
-        cur += (count * st)
+    tokens = 0
+    path = []
+    while tokens < 2*l:
+        line = raw_input().split()
+        tokens += len(line)
+        path += line
 
-    x,y,o = 0,0,0
+    for i in range(len(path)/2):
+        cur += int(path[2*i+1]) * path[2*i]
+
+    x, y, o = 0, 0, 0
 
     d = dict()
     e = dict()
 
     for s in cur:
         if s == 'F':
-            x,y,o = adv(x,y,o)
+            x, y, o = adv(x, y, o)
         elif s == 'L':
-            x,y,o = left(x,y,o)
+            x, y, o = left(x, y, o)
         else:
-            x,y,o = right(x,y,o)
+            x, y, o = right(x, y, o)
  
     sum = 0
     for k in d:
@@ -88,21 +71,13 @@ def compute():
         e[k].sort()
         u = e[k][1:-1]
         for i in range(len(u)/2):
-            
             for j in range(u[2*i], u[2*i+1]):
                 mn,mx = d[j][0], d[j][-1]
                 if k < mn or k >= mx:
                     sum += 1
 
     return sum
-            
 
-file = sys.stdin
 
-n = int(file.readline())
-buffer = file.read().split()
-
-for i in range(n):
-
-    print "Case #" + str(i+1) + ":",
-    print compute()
+for i in range(input()):
+    print "Case #%d: %d" % (i+1, compute())
--- Round 3 2008/B.py
+++ Round 3 2008/B.py
@@ -3,109 +3,78 @@
 # Problem: Portal
 # Language: Python
 # Author: KirarinSnow
-# Usage: python thisfile.py < input.in > output.out
-# Comments: Breadth-first search. Extremely slow.
+# Usage: python thisfile.py <input.in >output.out
 
-import sys
-from math import *
-from string import *
-
-MAX = 100000000000
-
-
-def grab():
-    return int(file.readline())
-
-def grabs():
-    return map(int,file.readline().split())
-
-def removechars(str, chars):
-    return str.translate(maketrans('',''),chars)
-
-def table(height, width):
-    t = []
-    for i in range(height):
-	t.append([])
-	for j in range(width):
-	    t[i].append(0)
-    return t
 
 def compute():
 
     def check(i,j):
         return i >= 0 and j >= 0 and i < r and j < c and room[i][j] != '#' 
 
-    r,c = map(int,file.readline().split())
+    r, c = map(int, raw_input().split())
 
-    room  = []
-    for i in range(r):
-        room.append(file.readline()[:-1])
+    room  = [raw_input() for i in range(r)]
 
     source = -1
     cake = -1
-    noportal = [-1,-1]
+    noportal = [-1, -1]
 
     for i in range(r):
         for j in range(c):
             if room[i][j] != '#':
-                if room[i][j]=='O':
-                    source = [[i,j],noportal]
-                if room[i][j]=='X':
-                    cake = [[i,j],noportal]
+                if room[i][j] == 'O':
+                    source = [[i, j], noportal]
+                if room[i][j] == 'X':
+                    cake = [[i, j], noportal]
 
-    nodes = [[-2,0],source]
-    visited = []
+    nodes = [[-2, 0], source]
+    vis = []
     dist = 0
 
-    while (len(nodes)>0):
+    while len(nodes) > 0:
         
-        curren3t = nodes.pop(0)
+        currentp = nodes.pop(0)
         
-        if curren3t[0] == -2:
-            dist = curren3t[1]
+        if currentp[0] == -2:
+            dist = currentp[1]
             continue
 
-        visited.append(curren3t)
-        current = [curren3t[0][0],curren3t[0][1]]
-        portal = curren3t[1]
+        vis.append(currentp)
+        current = [currentp[0][0], currentp[0][1]]
+        portal = currentp[1]
 
         if current == cake[0]:
             return str(dist)
-        ck = [noportal,portal]
+        ck = [noportal, portal]
 
-        for z in [[0,1],[0,-1],[1,0],[-1,0]]:
-            zz = [current[0],current[1]]
-            while check(zz[0]+z[0],zz[1]+z[1]):
+        for z in ([0, 1], [0, -1], [1, 0], [-1, 0]):
+            zz = [current[0], current[1]]
+            while check(zz[0]+z[0], zz[1]+z[1]):
                 zz[0] += z[0]
                 zz[1] += z[1]
-            if (not (zz in ck)) and zz != current:
+            if zz not in ck and zz != current:
                 ck.append(zz)
    
-        nodes.append([-2,dist+1])
-        for z in [[0,1],[0,-1],[1,0],[-1,0]]:
-            dest = [z[0]+current[0],z[1]+current[1]]
-            if check(dest[0],dest[1]):
-                for i in range(len( ck)):
-                    if (not ([dest,ck[i]] in visited)) and (not ([dest,ck[i]] in nodes)):
-                        nodes.append([dest,ck[i]])
+        nodes.append([-2, dist+1])
+        for z in ([0, 1], [0, -1], [1, 0], [-1, 0]):
+            dest = [z[0]+current[0], z[1]+current[1]]
+            if check(dest[0], dest[1]):
+                for i in range(len(ck)):
+                    if [dest, ck[i]] not in vis and [dest, ck[i]] not in nodes:
+                        nodes.append([dest, ck[i]])
 
         p = 0
-        for z in [[0,1],[0,-1],[1,0],[-1,0]]:
-            if not check(z[0]+current[0],z[1]+current[1]):
+        for z in ([0, 1], [0, -1], [1, 0], [-1, 0]):
+            if not check(z[0]+current[0], z[1]+current[1]):
                 p += 1
         if p > 0:
             for i in range(len(ck)):
-                if ((not ([ck[i],noportal] in visited)) and (not ([ck[i],noportal] in nodes)) and ck[i] != noportal):
+                if [ck[i], noportal] not in vis and \
+                        [ck[i], noportal] not in nodes and ck[i] != noportal:
                     nodes.append([ck[i],noportal])
 
     return "THE CAKE IS A LIE"
 
 
-file = sys.stdin
-
-n = int(file.readline())
-
-for i in range(n):
-    s = "Case #" + str(i+1) + ": "
-    s+= compute()
-    print s
+for i in range(input()):
+    print "Case #%d: %s" % (i+1, compute())
