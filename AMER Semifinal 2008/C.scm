d42ecf4b903b65a3e97e627f159f25b434ffef7e
KirarinSnow <kirarinsnow@uchicago.edu>
2010-11-22 00:00:05 -0600

    Test Passing Probability

--- AMER Semifinal 2008/C.scm
+++ AMER Semifinal 2008/C.scm
@@ -7,18 +7,18 @@
 (use-modules (srfi srfi-1) (ice-9 format))
 
 
-(define (truncate lst len)
+(define (trunc lst len)
   (take lst (min (length lst) len)))
 
 (define (compute)
   (let ((m (read)))
     (let loop
 	((q (read))
-	 (top (truncate (sort (list (read) (read) (read) (read)) >) m)))
+	 (top (trunc (sort (list (read) (read) (read) (read)) >) m)))
       (if (= q 1)
 	  (reduce + 0 top)
 	  (loop (1- q)
-		(truncate
+		(trunc
 		 (sort 
 		  (concatenate
 			 (map (lambda (x) (map (lambda (y) (* x y)) top))
