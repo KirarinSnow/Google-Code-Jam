171ea60e87b94a97ac3dd49884d9fa295cb2c652
KirarinSnow <kirarinsnow@uchicago.edu>
2010-12-17 20:36:37 +0900

    cleanup

--- Round 1C 2010/A.py
+++ Round 1C 2010/A.py
@@ -11,13 +11,13 @@ def compute():
     r = []
     
     for i in range(n):
-        r.append(map(int,raw_input().split()))
+        r.append(map(int, raw_input().split()))
 
     c = 0
     for i in range(n):
         for j in range(i):
             if (r[i][0] - r[j][0]) * (r[i][1] - r[j][1]) < 0:
-                c+=1
+                c += 1
     return c
 
 for i in range(input()):
--- Round 1C 2010/B.py
+++ Round 1C 2010/B.py
@@ -5,11 +5,12 @@
 # Author: KirarinSnow
 # Usage: python thisfile.py <input.in >output.out 
 
+
 from math import *
 
 def compute():
     l, p, c = map(int, raw_input().split())
-    return ceil(log(ceil(log((p+0.0)/l,c)),2))
+    return ceil(log(ceil(log((p+0.0)/l, c)), 2))
 
 for i in range(input()):
     print "Case #%d: %d" % (i+1, compute())
